06f060df2679f3994c18fd4e71790d2bec279bae
Eric Liu <eric7liu562@gmail.com>
2023-11-30 11:30:55 -0500

    score keeper default version complete

--- datapathFSM/scoreKeeper.v
+++ datapathFSM/scoreKeeper.v
@@ -3,6 +3,15 @@ module scoreKeeper (clock, scoreReset, moleHit, score);
     input scoreReset;
     input [2:0] hit;
     output reg [7:0] score;
+    initial begin
+        score = 5'b00000;
+    end
 
-
+    always @(posedge clock or posedge scoreReset) begin
+        if (scoreReset) begin
+            score <= 0;
+        end else begin
+            score <= score + 1;  //default, change it sto FSM with states later
+        end
+    end
 endmodule
