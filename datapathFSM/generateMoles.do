08dfdf9a8109c95a10c6da5be8e846e7076701a5
Eric Liu <eric7liu562@gmail.com>
2023-11-30 17:38:03 -0500

    fixed for enable

--- datapathFSM/generateMoles.do
+++ datapathFSM/generateMoles.do
@@ -11,16 +11,18 @@ force {clock} 0 0, 1 10ns -r 20ns
 
 force {reset} 1
 force {enable} 0
-
 run 40ns
+
 force {reset} 0
+run 20ns
 
-
-force {enable} 1
-run 40ns
+force {enable} 1 0, 0 10ns -r 40ns
+run 100ns
 
 force {enable} 0
 run 100ns
 
-force {enable} 1
+force {enable} 1 0, 0 10ns -r 40ns
+run 100ns
+
 run 100ns
--- datapathFSM/generateMoles.v
+++ datapathFSM/generateMoles.v
@@ -8,35 +8,37 @@ module generateMoles (clock, reset, enable, molesGenerated);
 endmodule
 
 module pseudo_rng(clock, reset, generateEn, output_data);
-    input clock, reset, generateEn;
-    output reg [4:0] output_data;
-
-    reg [2:0] temp_data;
-    parameter uppermax = $clog2(10000000);
-    reg [uppermax -1:0] counter;
-
-    always @(posedge clock) begin
-        if (reset) begin
-            counter <= 0;
-            output_data <= 0;
-        end else if (generateEn) begin
-            if (counter < 10000000)
-                counter <= counter + 1;
-            else
-                counter <= 0;
-            
-            temp_data <= counter % 5; 
-
-            case(temp_data) // one hot encoding
-                3'b000: output_data <= 5'b00001;
-                3'b001: output_data <= 5'b00010;
-                3'b010: output_data <= 5'b00100;
-                3'b011: output_data <= 5'b01000;
-                3'b100: output_data <= 5'b10000;
-                default: output_data <= 5'b00000;
-            endcase
-        end else begin
-            output_data <= 5'b00000;
-        end
+	// pseudo rng relying on fast clocks
+	input clock, reset, generateEn;
+	output reg [4:0] output_data;
+	
+	reg [2:0] temp_data;
+	parameter uppermax = $clog2(10000000);
+	reg [uppermax -1:0] counter;
+	
+	always@(posedge clock) begin
+		if (reset) begin
+			counter <= 0;
+			temp_data <= 0;
+		end 
+		else begin
+			if (counter < 10000000 && counter >= 0)
+				counter <= counter + 1;
+			else
+				counter <= 0;
+		end
+	end
+	
+	always@(posedge generateEn) begin
+		temp_data = counter % 5; // 0-4
+		
+		case(temp_data) // one hot encoding
+			3'b000: output_data <= 5'b00001;
+			3'b001: output_data <= 5'b00010;
+			3'b010: output_data <= 5'b00100;
+			3'b011: output_data <= 5'b01000;
+			3'b100: output_data <= 5'b10000;
+			default: output_data <= 5'b00000;
+		endcase
     end
 endmodule
